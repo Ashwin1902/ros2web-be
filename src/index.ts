aacb45d133ea07ab4be033a401c9545e7ea2816c
Ashwin1902 <ashwinbansal19feb@gmail.com>
2025-01-09 10:48:41 +0530

    task 1,3

--- src/index.ts
+++ src/index.ts
@@ -2,28 +2,50 @@ import express, {Request,Response} from "express"
 import fs from 'fs';
 import path from 'path';
 import cors from 'cors';
+import { randomUUID } from "crypto";
 const app=express()
 const dataFilePath = path.join(__dirname, 'data.json');
 app.use(express.json())
 app.use(cors())
+
+interface buttonInterface{
+    id:string,
+    name:string,
+    speed:number,
+    acceleration:number,
+    wait:number,
+}
+
 app.post('/addButton',(req:Request,res:Response)=>{
     try {
-        const { buttonType, value } = req.body;
-    
-        if (!buttonType || !value) {
-            return res.status(400).json({ message: 'buttonType and value are required.' });
-        }
-        const newButton = { buttonType, value };
+        const id=randomUUID();
+        const { name,speed,acceleration,wait } = req.body;
+        const newButton:buttonInterface = {name,speed,acceleration,wait,id};       
         const data = fs.readFileSync(dataFilePath,'utf-8');
         const currObject= JSON.parse(data);
         currObject.buttons.push(newButton)
-        // console.log(myObject);
         fs.writeFileSync(dataFilePath, JSON.stringify(currObject, null, 2), 'utf-8');
-        // console.log(data);
+        //console.log(newButton);
+        return res.status(200).json({ message: 'Button added successfully', data: newButton });
+    } catch (error) {
+        return res.status(400).json({"error: ":error});
         
-        // const myObject= JSON.parse(data);
+    }
+   
+})
+
+app.put('/editButton',(req:Request,res:Response)=>{
+    try {
         
-        console.log(newButton);
+        const { name,speed,acceleration,wait,id } = req.body;
+        const data = fs.readFileSync(dataFilePath,'utf-8');
+        const currObject= JSON.parse(data);
+        const newButtonIdx=currObject.buttons.findIndex((btn:{id:string})=>btn.id==id)
+        if(newButtonIdx==-1) return res.status(400).json({ message: 'Button does not exist, cannot update'});      
+        const newButton:buttonInterface={name,speed,acceleration,wait,id}
+        currObject.buttons[newButtonIdx]=newButton
+        fs.writeFileSync(dataFilePath, JSON.stringify(currObject, null, 2), 'utf-8');
+        console.log("Button edited",newButton);
         return res.status(200).json({ message: 'Button added successfully', data: newButton });
     } catch (error) {
         return res.status(400).json({"error: ":error});
@@ -85,4 +107,3 @@ app.get('/getAllButtons',(req:Request,res:Response)=>{
 app.listen(5000,()=>{
     console.log("server is listening on port 5000")
 })
-
